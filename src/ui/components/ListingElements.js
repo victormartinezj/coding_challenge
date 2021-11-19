3fa2cd2cc682aaba5b929c920ad315377e49d074
victor <udemyreactfull@gmail.com>
2021-11-18 19:26:18 -0600

    Search bar design

--- src/ui/components/ListingElements.js
+++ src/ui/components/ListingElements.js
@@ -59,28 +59,31 @@ const ListingElements = ({ universities }) => {
 
     return (
         <div>
-            <div>
-                Search:{" "}
-                <input
-                    type="text"
-                    id="search-university"
-                    value={filterUniversities}
-                    onChange={e => setFilterUniversities(e.target.value)}
-                />
+            <div class="bg-blue-700 p-4 rounded mb-6 flex justify-between items-center flex-wrap  text-white ">
                 <div>
+                    Search:{" "}
+                    <input
+                        class="rounded px-4 py-1 text-blue-600"
+                        type="text"
+                        id="search-university"
+                        value={filterUniversities}
+                        onChange={e => setFilterUniversities(e.target.value)}
+                    />
+                </div>
+                <div class="space-x-2 w-14 py-2">
                     <button
+                        class="rounded py-2 px-2 border border-gray-200 "
                         onClick={() => {
                             setSortingElements("order");
                         }}>
-                        Order alphabetically
+                        A-Z
                     </button>
-                </div>
-                <div>
                     <button
+                        class="rounded py-2 px-2 border border-gray-200 "
                         onClick={() => {
                             setSortingElements("reverse");
                         }}>
-                        Order reverse
+                        Z-A
                     </button>
                 </div>
             </div>
